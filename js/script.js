ce91e5a5b8c34938bb0ece378421035234221539
aravind-9438 <vindara987@gmail.com>
2021-03-07 15:37:24 +0530

    code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("submit").onclick=function(){
+    var a,b,c,d;
+a=document.getElementById("l1").value;
+b=document.getElementById("l2").value;
+c=document.getElementById("l3").value;
+d=document.getElementById("l4").value;
+  
+ }
